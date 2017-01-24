69e6c56338f4c8d0568239017d9c3b21ca7c8af1
dragonboyvny - bitbucket <vinnylee@gmail.com>
2017-01-24 10:31:07 +0000

    PHP 101 lecture 2-3
    PHP and HTML basics
    booooorrrrring…

---
+++ T01/L02/welcome.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Web Programming</title>
+</head>
+<body>
+
+	<?php
+		//This is a PHP comment
+	echo "<h2>PHP 101</h2>";
+		echo  "<p>Welcome to Web Programming";
+		echo  "<p>";
+		$i=1;
+		//This is a PHP while loop
+		while($i<=5) {
+  			echo "The value of the loop variable (i) is = " . $i . "<br />";
+	  		$i++;
+	  	}
+	?>
+</body>
+</html> 
---
+++ T01/L03/php_multiblocks.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>PHP_Multiblocks</title>
+</head>
+<body>
+<h1>This is the first PHP block</h1>
+<?php
+	//assign values to some variables
+	$i=1;  
+	$x=10;
+	echo  "<p>Welcome to Web Programming";
+	echo  '<p>The value of $i is'.$i.' another literal string'; //using single quote \' creates a literal string (exactyl is shown)
+	echo  "<p>The value of $x is".$x; //concatenate using fullstop
+?>
+
+<h1>This is the second PHP block</h1>
+
+<!--
+<script language="php">		//notice the use of the SCRIPT tag
+	//use the variables to control a loop
+	while($i <= $x){
+	  echo " $x*$i= " . $i*$x . "<br>";
+	  $i++;
+  	}  
+</script>
+-->
+
+
+<?php		
+	while($i <= $x){
+	  echo " $x*$i= " . $i*$x . "<br>";
+	  $i++;
+  	}  
+?>
+</body>
+</html> 
